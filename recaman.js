4887cb3b7ed2481953cf0c3dcbf486da545bafef
Erik Thuning <thuning@dsv.su.se>
2021-09-16 23:58:32 +0200

    Modularized the code a bit

--- recaman.js
+++ recaman.js
@@ -1,70 +1,92 @@
-var c = document.getElementById('recaman')
+(function() {
+    function recaman(canvas) {
+        var width = canvas.width = canvas.clientWidth
+        var height = canvas.height = canvas.clientHeight
 
-c.width = document.body.clientWidth
-c.height = document.body.clientHeight
-
-var x_zero = 10
-var y_zero = c.height / 2
-
-var scale = 2
-
-function set_scale(event) {
-    if(event.deltaY < 0) {
-        scale = scale / 2
-    } else {
-        scale = scale * 2
-    }
-    draw()
-}
-
-function draw() {
-    document.getElementById('scale').textContent = scale
-    var p = c.getContext('2d')
-    p.strokeStyle = 'black'
-    p.clearRect(0, 0, c.width, c.height)
-    p.lineWidth = 1
-    p.beginPath()
-    p.moveTo(x_zero, y_zero)
-    p.lineTo(c.width, y_zero)
-    p.stroke()
-    
-    p.beginPath()
-    p.moveTo(x_zero, y_zero - 10)
-    p.lineTo(x_zero, y_zero + 10)
-    p.stroke()
-    
-    var step = 1
-    var log = [0]
-    var up = true
-    for(var i = 0; i < c.width * (2/scale);) {
-        var next = i - step
-        var forwards = false
-        if(next < 0 || log.indexOf(next) != -1) {
-            next = i + step
-            forwards = true
+        var scale = 2
+        var x_zero = 10
+        var y_zero = height/2
+        if(canvas.dataset.scale) {
+            scale = Number(canvas.dataset.scale)
+        }
+        if(canvas.dataset.xzero) {
+            x_zero = Number(canvas.dataset.xzero)
+        }
+        if(canvas.dataset.yzero) {
+            y_zero = Number(canvas.dataset.yzero)
         }
 
-        var color = "hsla("+ step +", 100%, 50%, .9)"
-        p.strokeStyle = color
+        // Preliminaries
+        var p = canvas.getContext('2d')
+        p.strokeStyle = 'black'
+        p.clearRect(0, 0, width, height)
+        p.lineWidth = 1
 
-        var radius = step/2
-        var offset = forwards ? radius : -radius
-        var center = x_zero + (i + offset) * scale
-
+        // X-axis
         p.beginPath()
-        if(forwards) {
-            p.arc(center, y_zero, radius * scale, Math.PI, 0, up)
-        } else {
-            p.arc(center, y_zero, radius * scale, 0, Math.PI, !up)
-        }
+        p.moveTo(x_zero, y_zero)
+        p.lineTo(width, y_zero)
         p.stroke()
-        
-        log.push(next)
-        step += 1
-        up = !up
-        i = next
-    }
-}
 
-document.addEventListener('DOMContentLoaded', draw)
-document.addEventListener('wheel', set_scale)
+        // Y-stub
+        p.beginPath()
+        p.moveTo(x_zero, y_zero - 10)
+        p.lineTo(x_zero, y_zero + 10)
+        p.stroke()
+
+        var step = 1
+        var log = [0]
+        var up = true
+        for(var i = 0; i < width * (2/scale);) {
+            var next = i - step
+            var forward = false
+            if(next < 0 || log.indexOf(next) != -1) {
+                next = i + step
+                forward = true
+            }
+
+            var color = "hsla("+ step +", 100%, 50%, .9)"
+            p.strokeStyle = color
+
+            var radius = step/2
+            var offset = forward ? radius : -radius
+            var center = x_zero + (i + offset) * scale
+
+            p.beginPath()
+            if(forward) {
+                p.arc(center, y_zero, radius * scale, Math.PI, 0, up)
+            } else {
+                p.arc(center, y_zero, radius * scale, 0, Math.PI, !up)
+            }
+            p.stroke()
+
+            log.push(next)
+            step += 1
+            up = !up
+            i = next
+        }
+    }
+
+    function set_scale(event) {
+        var factor = 2
+        var canvas = event.currentTarget
+        if(event.deltaY < 0) {
+            canvas.dataset.scale = canvas.dataset.scale * factor
+        } else {
+            canvas.dataset.scale = canvas.dataset.scale / factor
+        }
+        recaman(canvas)
+    }
+
+    function init(event) {
+        var canvases = document.querySelectorAll('.recaman')
+        canvases.forEach(function(canvas) {
+            canvas.addEventListener('wheel', set_scale)
+            recaman(canvas)
+        })
+        window.addEventListener('resize', function(event) {
+            canvases.forEach(function(canvas) {
+                recaman(canvas)})})
+    }
+    document.addEventListener('DOMContentLoaded', init)
+})()
